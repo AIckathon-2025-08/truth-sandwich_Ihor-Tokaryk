5c2c7646794a5eee8e0c1cba7386228c68002171
ihor-tokaryk <ihor_tokaryk@epam.com>
2025-08-14 16:21:57 +0200

    do not expose lie while active

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,9 +1,9 @@
 require_relative 'application_controller'
 
 class ResultsController < ApplicationController
-  # Show live results for current active game
+  # Show live results for current active game or most recent game
   get '/' do
-    @game = Game.current_active
+    @game = Game.current_active || Game.order(created_at: :desc).first
     
     if @game.nil?
       erb :'results/no_active_game'
@@ -11,6 +11,7 @@ class ResultsController < ApplicationController
       @vote_counts = @game.vote_counts
       @total_votes = @game.total_votes
       @statements = @game.statements_for_results
+      @show_lie = !@game.active? # Only show lie when game is inactive
       erb :'results/index'
     end
   end
@@ -19,7 +20,7 @@ class ResultsController < ApplicationController
   get '/api' do
     content_type :json
     
-    @game = Game.current_active
+    @game = Game.current_active || Game.order(created_at: :desc).first
     
     if @game.nil?
       { active: false }.to_json
@@ -33,9 +34,11 @@ class ResultsController < ApplicationController
           id: @game.id,
           name: @game.name,
           position: @game.position,
+          active: @game.active?,
           statements: statements,
           vote_counts: vote_counts,
-          total_votes: @game.total_votes
+          total_votes: @game.total_votes,
+          show_lie: !@game.active? # Only show lie when game is inactive
         }
       }.to_json
     end
